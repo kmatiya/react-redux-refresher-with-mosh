d18deb3dc27d437f92429f6bacc106e33bc1f8e2
kmatiya <khormatiya>
2021-01-04 21:20:40 +0200

    Add function for action dispatcher

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 import * as actions from "./actionTypes";
+import { bugAdded } from "./actions";
+
 const unscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug1"));
 
 unscribe();
 
